5cd5f898351bd3ee056aef6c7a86f73aada72315
Cristian <cristianma98@gmail.com>
2022-06-09 14:21:45 +0200

    Commit di inizio progetto

---
+++ src/main/java/it/prova/gestionesocietaspringdatamaven/GestionesocietaspringdatamavenApplication.java
@@ -0,0 +1,13 @@
+package it.prova.gestionesocietaspringdatamaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionesocietaspringdatamavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionesocietaspringdatamavenApplication.class, args);
+	}
+
+}
